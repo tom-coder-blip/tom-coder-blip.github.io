4b19f3a9d0093b79e9781c6ec5c45297df0e056e
Tom Vuma <vumatom10@gmail.com>
2025-07-17 19:12:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     rootMargin: "0px 0px -100px 0px"
   };
 
-  const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+  const appearOnScroll = new IntersectionObserver(function (entries, observer) {
     entries.forEach(entry => {
       if (!entry.isIntersecting) return;
       entry.target.classList.add("appear");
@@ -18,4 +18,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   faders.forEach(fader => appearOnScroll.observe(fader));
   sliders.forEach(slider => appearOnScroll.observe(slider));
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('.click-flip').forEach(card => {
+    card.addEventListener('click', () => {
+      card.classList.toggle('flipped');
+    });
+
+    card.querySelectorAll('a').forEach(link => {
+      link.addEventListener('click', e => e.stopPropagation());
+    });
+  });
 });
